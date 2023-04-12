ce63a0d36bc150ebabfb6769e11a3f393079d977
rosamsierrap <120815217+rosamsierrap@users.noreply.github.com>
2023-04-12 18:47:00 -0400

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 /usr/local/hadoop/sbin/stop-dfs.sh
-/usr/local/hadoop/sbin/stop-yarn.sh
-/usr/local/hadoop/sbin/mr-jobhistory-daemon.sh stop historyserver
+/usr/local/spark/sbin/stop-all.sh
